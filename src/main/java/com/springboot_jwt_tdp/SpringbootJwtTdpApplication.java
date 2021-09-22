ec41c60bafa1fb991cd3c6f5c3d074d48bcc200c
Phuoc <tdphuoc77@gmail.com>
2021-09-22 08:12:04 +0700

    first commit

---
+++ src/main/java/com/springboot_jwt_tdp/SpringbootJwtTdpApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_jwt_tdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtTdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtTdpApplication.class, args);
+    }
+
+}
